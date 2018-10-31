35c972cd6418c26a2a41ea8eb1d5f58419f1fa76
Andrew <herobrin3000@gmail.com>
2018-10-31 21:28:44 +0300

    add 2 level

--- l03_01.rb
+++ l03_01.rb
@@ -7,44 +7,76 @@ key_file = "My Project-cf8c099f9b91.json"
 # Instantiates a client
 translate = Google::Cloud::Translate.new project: project_id, keyfile: key_file
 
-arr = File.readlines('noun_dictionary.txt').each {|l| l.chomp!}
+flag = true
+points = 0
+puts "Rules: for every new letter you get three points, for wrong answer -1 point, for all word - every '*' 4 point, and then -1"
+puts "You have 3 attempts to guess all word, then penalty -5 points"
+while flag
+    arr = File.readlines('noun_dictionary.txt').each {|l| l.chomp!}
 
-num = rand(arr.length)
+    num = rand(arr.length)
 
-# The text to translate
-text = arr[num].downcase
+    # The text to translate
+    text = arr[num].downcase
 
-# The target language
-target = "ru"
+    # The target language
+    target = "ru"
 
-# Translates some text into Russian
-translation = translate.translate text, to: target
+    # Translates some text into Russian
+    translation = translate.translate text, to: target
 
-if (text.chomp != translation.to_s.chomp.downcase)
-    puts text
-    puts "Перевод: #{translation}"
-    now = "*" * text.length
-    used = ""
-    while now.include? "*"
-        puts now
-        puts "Used: #{used}"
-        simb = gets.chomp
-        if text.include? simb
-            i = 0
-            while i < text.length
-            if text[i] == simb
-                now[i] = simb
+    if (text.chomp != translation.to_s.chomp.downcase)
+        puts text
+        trying = 3
+        puts "Перевод: #{translation}"
+        now = "*" * text.length
+        used = ""
+        while now.include? "*"
+            puts now
+            puts "Used: #{used}"
+            puts "All word attempts: #{trying}"
+            simb = gets.chomp
+            if (simb.length != 1)
+                if (trying > 0)
+                    trying -= 1
+                else
+                    points -= 5
+                end
+            elsif text.include? simb
+                i = 0
+                if (simb.length == 1)
+                    points += 3
+                end
+                while i < text.length
+                if text[i] == simb
+                    now[i] = simb
+                end
+                i+=1
+                end
+            elsif simb.length == 1
+                used += simb
+                points -= 1
             end
-            i+=1
+            if simb.chomp == text.chomp
+                points += now.count('*') * 4 - 1
+                now = ""
             end
-        elsif simb.length == 1
-            used += simb
         end
-        if simb.chomp == text.chomp
-            now = ""
+        puts "Good Luck, Have Fun, You Win!"
+        puts "Your points: #{points}"
+        flag1 = true
+        while flag1
+            puts "Continue? (+/-)"
+            answer = gets.chomp
+            if (answer == "+")
+                flag1 = false
+            elsif (answer == "-")
+                flag1 = false
+                flag = false
+            else
+                puts "Incorrect"
+            end
         end
     end
-    puts "Good Luck, Have Fun, You Win!"
-else
-    puts "You Unlucky, try one more time"
+
 end
