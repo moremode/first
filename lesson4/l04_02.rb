feab1c83d3abcf300a38563ea65180fbbf1d85b4
moremode <herobrin3000@gmail.com>
2018-11-11 18:21:48 +0300

    change2n2

--- lesson4/l04_02.rb
+++ lesson4/l04_02.rb
@@ -1,15 +1,22 @@
+require 'date'
+
 print 'Investment in month: '
 in_month = gets.chomp.to_i
 print 'When you take away year: '
 year_end = gets.chomp.to_i
 print 'When you take away month: '
 month_end = gets.chomp.to_i
-monthes = (year_end - 2018) * 12 + (month_end - 11)
+
+d = DateTime.now
+
+monthes = (year_end - d.strftime("%Y").to_i) * 12 + (month_end - d.strftime("%m").to_i)
 
 invoice = in_month
 
 for index in 1..monthes
    invoice = invoice * (1 + 0.07/12) + in_month
 end
+
+invoice = invoice - in_month
 
 puts "In the end of the time: #{invoice.round(2)}"
